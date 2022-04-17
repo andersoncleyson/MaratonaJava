d8e0c1e253de66083fe0dc0ec96dc42c22069a38
Anderson <andersoncleyson@protonmail.com>
2022-04-17 10:48:08 -0300

    modificador final pt2

--- src/academy/devdojo/maratonajava/javacore/JmodificadorFinal/tests/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/JmodificadorFinal/tests/CarroTest01.java
@@ -1,9 +1,17 @@
 package academy.devdojo.maratonajava.javacore.JmodificadorFinal.tests;
 
 import academy.devdojo.maratonajava.javacore.JmodificadorFinal.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.JmodificadorFinal.dominio.Comprador;
 
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
+
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+
+        carro.COMPRADOR.setNome("Ivin");
+
+        System.out.println(carro.COMPRADOR);
     }
 }
