264668725d85df00f5de9b452424d97e1f1201d9
Anderson <andersoncleyson@protonmail.com>
2022-04-17 10:31:36 -0300

    modificador final pt1

---
+++ src/academy/devdojo/maratonajava/javacore/JmodificadorFinal/tests/CarroTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.JmodificadorFinal.tests;
+
+import academy.devdojo.maratonajava.javacore.JmodificadorFinal.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+    }
+}
