749421f18630011796b29ff46af13b25c27cfa84
Anderson <andersoncleyson@protonmail.com>
2022-04-18 06:27:42 -0300

    Enumeração pt01

---
+++ src/academy/devdojo/maratonajava/javacore/JmodificadorFinal/dominio/Subaru.java
@@ -0,0 +1,5 @@
+package academy.devdojo.maratonajava.javacore.JmodificadorFinal.dominio;
+
+public class Subaru extends Carro{
+
+}
--- src/academy/devdojo/maratonajava/javacore/JmodificadorFinal/tests/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/JmodificadorFinal/tests/CarroTest01.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.JmodificadorFinal.tests;
 
 import academy.devdojo.maratonajava.javacore.JmodificadorFinal.dominio.Carro;
 import academy.devdojo.maratonajava.javacore.JmodificadorFinal.dominio.Comprador;
+import academy.devdojo.maratonajava.javacore.JmodificadorFinal.dominio.Subaru;
 
 public class CarroTest01 {
     public static void main(String[] args) {
@@ -10,8 +11,13 @@ public class CarroTest01 {
         System.out.println(Carro.VELOCIDADE_LIMITE);
         System.out.println(carro.COMPRADOR);
 
-        carro.COMPRADOR.setNome("Ivin");
+        carro.COMPRADOR.setNome("Irvin");
 
         System.out.println(carro.COMPRADOR);
+
+        Subaru subaru = new Subaru();
+
+        subaru.setNome("Bumblebee");
+        subaru.imprime();
     }
 }
