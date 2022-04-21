a1b417e5b4b3653c3b25ad6ca956522b71155ca5
Anderson <andersoncleyson@protonmail.com>
2022-04-21 12:09:39 -0300

    Classes abstradas

---
+++ src/academy/devdojo/maratonajava/javacore/LclassesAbstradas/tests/FuncionarioTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.LclassesAbstradas.tests;
+
+import academy.devdojo.maratonajava.javacore.LclassesAbstradas.dominio.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.LclassesAbstradas.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.LclassesAbstradas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+
+        Gerente gerente = new Gerente("Angela", 4000);
+
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Elliot", 20000);
+
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
