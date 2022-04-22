c256c7a12128c5f0eb70e0eaf301de08c2045fb4
Anderson <andersoncleyson@protonmail.com>
2022-04-22 06:32:14 -0300

    Implementando múltiplas interfaces

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -1,8 +1,18 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
-public class DatabaseLoader implements DataLoader {
+public class DatabaseLoader implements DataLoader, DataRemover {
     @Override
     public void load(){
         System.out.println("Loading data from database");
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("Deleted from database");
+    }
+
+    @Override
+    public void checkPermission() {
+        DataLoader.super.checkPermission();
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/FileLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/FileLoader.java
@@ -1,8 +1,18 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
-public class FileLoader implements DataLoader{
+public class FileLoader implements DataLoader, DataRemover{
     @Override
     public void load() {
         System.out.println("Loading data from files");
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("Deteted from database");
+    }
+
+    @Override
+    public void checkPermission() {
+        System.out.println("Checking permissions files");
     }
 }
