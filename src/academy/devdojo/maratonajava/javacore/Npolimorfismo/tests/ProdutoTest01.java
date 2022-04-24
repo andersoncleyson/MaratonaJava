4b67a9580f123ca28424488a54a306d6381a1cf8
Anderson <andersoncleyson@protonmail.com>
2022-04-24 19:37:00 -0300

    Parâmetros polimórficos

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/tests/ProdutoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/tests/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.tests;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,12 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("2AM", 5000);
         Tomate tomate = new Tomate("Tomate", 2);
+        Televisao televisao = new Televisao("LG 50\" ", 2500);
 
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("==========================");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("==========================");
+        CalculadoraImposto.calcularImposto(televisao);
     }
 }
